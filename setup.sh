0cf11610e81fe7c93eb6704f186d9a006fcfdf41
Phuc Nguyen <phucnn.work@gmail.com>
2024-05-19 21:19:15 +0800

    lazy vim config

--- setup.sh
+++ setup.sh
@@ -2,6 +2,13 @@ ln -s $(pwd)/zshrc $HOME/.zshrc
 ln -s $(pwd)/bashrc $HOME/.bashrc
 ln -s $(pwd)/aliasrc $HOME/.aliasrc
 
+#nvim 
+mv ~/.config/nvim{,.bak}
+mv ~/.local/share/nvim{,.bak}
+mv ~/.local/state/nvim{,.bak}
+mv ~/.cache/nvim{,.bak}
+ln -s $(pwd)/nvim $HOME/.config/nvim
+
 # setting up sdkman, my favorite JVM related sdk manager
 curl -s "https://get.sdkman.io" | bash
 
